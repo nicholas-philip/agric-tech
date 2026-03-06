0623984bcb27a4c674fd6b2ccacffe75a634f599
philip <philipnicholas386@gmail.com>
2026-03-06 16:24:51 +0000

    first commit

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, useRouter ,useSegment} from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'react-native';
 import 'react-native-reanimated';
@@ -11,6 +11,9 @@ export default function RootLayout() {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
   const colorScheme = useColorScheme();
+  const router = useRouter()
+  const segment = useSegment();
+
 
   const onAuthStateChanged = (user) => {
     setUser(user);
@@ -21,11 +24,15 @@ export default function RootLayout() {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
   }, []);
+ useEffect(() => {
+     if (initializing) return;
+const routeName = user ? 'tabs' : 'auth';
 
+},[user,initializing])
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
-        {/* The first screen in the stack will be the initial one. 
+        {/* The first screen in the stack will be the initial one.
             We'll set login as the entry point. */}
         <Stack.Screen name="(auth)" options={{ animation: 'fade' }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
